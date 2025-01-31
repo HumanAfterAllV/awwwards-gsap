4031398426c112c28141250794575acf07521728
HumanDev <motocycle9719@hotmail.com>
2025-01-31 15:26:12 -0600

    add(hero-section): homepage

---
+++ src/components/Button.tsx
@@ -0,0 +1,28 @@
+
+import { ReactNode } from "react";
+import clsx from "clsx";
+
+type ButtonProps = {
+    id: string;
+    title: string;
+    leftIcon?: ReactNode;
+    rightIcon?: ReactNode;
+    containerClass: string;
+}
+
+export default function Button({ id, title, leftIcon, rightIcon, containerClass }: ButtonProps): React.JSX.Element{
+    return(
+        <div id={id} className={clsx("group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full px-7 py-3 text-dark-500", containerClass)}>
+            {leftIcon}
+            <span className="relative incline-flex overflow-hidden font-general text-xs">
+                <div className="translate-y-0 skew-y-0 transition duration-500 group-hover:translate-y-[-160%] group-hover:skew-y-12">
+                    {title}
+                </div>
+                <div className="absolute translate-y-[164%] skew-y-12 transition duration-500 group-hover:translate-y-0 group-hover:skew-y-0">
+                    {title}
+                </div>
+            </span>
+            {rightIcon}
+        </div>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,28 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: {
+          50: "#DFDFF0",
+          75: "#DFDFF2",
+          100: "#F0F2FA",
+          300: "#4FB7DD"         
+        },
+        dark: {
+          500: "#010101"
+        },
+        violet:{
+          300: "#5724FF"
+        },
+        yellow: {
+          100: "#8E983F",
+          300: "#EDFF66"
+        }
+      },
+      fontFamily: {
+        robert: 'var(--robert), sans-serif',
+        zentry: 'var(--zentry), sans-serif',
+        general: 'var(--general), sans-serif',
+        circular: 'var(--circular), sans-serif',
       },
     },
   },
