197990acb455247d87176a1a1e2a1939a6e61d03
HumanDev <motocycle9719@hotmail.com>
2025-02-01 13:48:49 -0600

    add(features-section): homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Hero from "@components/Hero";
 import About from "@components/About";
 import Navbar from "@components/Navbar";
+import Features from "@components/Features";
 
 export default function Page(): React.JSX.Element{
     return(
-        <main className="relative min-h-screen w-screen overflow-x-hidden bg-zinc-600">
+        <main className="relative min-h-screen w-screen overflow-x-hidden">
             <Navbar /> 
             <Hero />
             <About />
+            <Features />
         </main>
     )
 }
